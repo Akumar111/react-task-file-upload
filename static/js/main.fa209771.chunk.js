4998cdd2743a559c685d97d26cc094b7cb0f3fa5
Anup Krishna Kumar <akumar11@mt.iitr.ac.in>
2021-04-15 19:35:59 +0530

    Updates

--- static/js/main.fa209771.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),r=n(3),s=n.n(r),o=(n(8),n(0));var l=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"Hello"})})};s.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(l,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.fa209771.chunk.js.map
